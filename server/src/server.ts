82ef4a48c1252c5e291011b0278c2f35a6d95184
Fernand <fernando.vgarcia@hotmail.com>
2020-06-02 22:35:14 -0300

    Controllers and routes created.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+import path from 'path';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    response.json([
-        'Fernando',
-        'Bruno',
-        'Felipe'
-    ]);
-});
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);

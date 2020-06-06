6fcde80d06404528124655862cb2948bb269106c
Fernand <fernando.vgarcia@hotmail.com>
2020-06-06 01:39:12 -0300

    Uploading images.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -10,6 +10,8 @@ import logo from '../../assets/logo.svg';
 import api from '../../services/api';
 import ibgeService from '../../services/ibge';
 
+import Dropzone from '../../components/Dropzone';
+
 interface Item {
     id: number;
     title: string;
@@ -41,6 +43,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -117,18 +120,22 @@ const CreatePoint = () => {
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
+        
+        const data = new FormData();
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
 
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
+        
         await api.post('points', data);
 
         alert('Ponto de coleta criado!');
@@ -149,6 +156,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>

681c0f43e9cfd5ef62d3d3b601afc2e35e589004
Fernand <fernando.vgarcia@hotmail.com>
2020-06-05 23:46:26 -0300

    Mobile connected with backend.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 import {AppLoading} from 'expo';
 
-import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
 
 import Routes from './src/routes';
 
@@ -15,13 +15,13 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading/>
   }
 
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Routes />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+      <Routes/>
     </>
   );
 }
